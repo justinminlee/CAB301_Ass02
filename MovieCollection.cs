99d884c5190a0789e4f463c8ff6f0b619ce4acf2
Justin Lee <smsc19970205@gmail.com>
2023-05-26 23:15:21 +1000

    first commit

---
+++ MovieCollection.cs
@@ -0,0 +1,307 @@
+// CAB301 - assignment 2
+// An implementation of MovieCollection ADT
+// 2023
+
+
+using System;
+
+//A class that models a node of a binary search tree
+//An instance of this class is a node in the binary search tree 
+public class BTreeNode
+{
+	private IMovie movie; // movie
+	private BTreeNode? lchild; // reference to its left child 
+	private BTreeNode? rchild; // reference to its right child
+
+	public BTreeNode(IMovie movie)
+	{
+		this.movie = movie;
+		lchild = null;
+		rchild = null;
+	}
+
+	public IMovie Movie
+	{
+		get { return movie; }
+		set { movie = value; }
+	}
+
+	public BTreeNode? LChild
+	{
+		get { return lchild; }
+		set { lchild = value; }
+	}
+
+	public BTreeNode? RChild
+	{
+		get { return rchild; }
+		set { rchild = value; }
+	}
+}
+
+// invariant: no duplicate movie in this movie collection
+public class MovieCollection : IMovieCollection
+{
+	private BTreeNode? root; // the root of the binary search tree in which movies are stored 
+	private int count; // the number of movies currently stored in this movie collection 
+
+
+
+	public int Number { get { return count; } }
+
+	// constructor - create an empty movie collection
+	public MovieCollection()
+	{
+		root = null;
+		count = 0;	
+	}
+
+	public bool IsEmpty()
+	{
+
+		//To be completed by students
+		if (count == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+	public bool Insert(IMovie movie)
+	{
+
+        //To be completed by students
+        if (root == null)
+        {
+            root = new BTreeNode(movie);
+            count++;
+            return true;
+        }
+
+        BTreeNode curr = root;
+        BTreeNode prev = null;
+        while (curr != null)
+        {
+            if (movie.CompareTo(curr.Movie) == 0)
+            {
+                return false;
+            }
+            else if (movie.CompareTo(curr.Movie) < 0)
+            {
+                prev = curr;
+                curr = curr.LChild;
+            }
+            else
+            {
+                prev = curr;
+                curr = curr.RChild;
+            }
+        }
+
+        if (movie.CompareTo(prev.Movie) < 0)
+        {
+            prev.LChild = new BTreeNode(movie);
+        }
+        else
+        {
+            prev.RChild = new BTreeNode(movie);
+        }
+
+        count++;
+        return true;
+
+    }
+
+
+    public bool Delete(IMovie movie)
+	{
+        //To be completed by students
+
+        // If empty, don't do anything
+        if (root == null) 
+        {
+            return false;
+        }
+
+        // Try to find the movie in the tree
+        BTreeNode? parent = null;
+        BTreeNode? current = root;
+        while (current != null)
+        {
+            int cmp = movie.CompareTo(current.Movie);
+            if (cmp < 0)
+            {
+                parent = current;
+                current = current.LChild;
+            }
+            else if (cmp > 0)
+            {
+                parent = current;
+                current = current.RChild;
+            }
+            else
+            {
+                // Found the node that contains the movie to be deleted
+                if (current.LChild == null && current.RChild == null)
+                {
+                    // Case 1: The node to be deleted is a leaf node
+                    if (parent == null)
+                    {
+                        root = null;
+                    }
+                    else if (parent.LChild == current)
+                    {
+                        parent.LChild = null;
+                    }
+                    else
+                    {
+                        parent.RChild = null;
+                    }
+                }
+                else if (current.LChild == null)
+                {
+                    // Case 2: The node to be deleted has one child (right child)
+                    if (parent == null)
+                    {
+                        root = current.RChild;
+                    }
+                    else if (parent.LChild == current)
+                    {
+                        parent.LChild = current.RChild;
+                    }
+                    else
+                    {
+                        parent.RChild = current.RChild;
+                    }
+                }
+                else if (current.RChild == null)
+                {
+                    // Case 2: The node to be deleted has one child (left child)
+                    if (parent == null)
+                    {
+                        root = current.LChild;
+                    }
+                    else if (parent.LChild == current)
+                    {
+                        parent.LChild = current.LChild;
+                    }
+                    else
+                    {
+                        parent.RChild = current.LChild;
+                    }
+                }
+                else
+                {
+                    // Case 3: The node to be deleted has two children
+                    BTreeNode? minNode = current.RChild;
+                    while (minNode.LChild != null)
+                    {
+                        minNode = minNode.LChild;
+                    }
+                    current.Movie = minNode.Movie;
+                    current = minNode;
+                    parent = null; // Reset parent to null to delete the minNode so reset parent to null
+                }
+
+                count--;
+                return true;
+            }
+        }
+
+        // If not found, don't do anything
+        return false;
+
+    }
+
+
+
+	public IMovie? Search(string movietitle)
+	{
+        //To be completed by students
+
+        // If empty, return null
+        if (count == 0) return null;
+
+        // Traverse the binary search tree to find the node with the matching movie title
+        BTreeNode? current = root;
+        while (current != null)
+        {
+            int cmp = String.Compare(movietitle, current.Movie.Title);
+            if (cmp == 0) return current.Movie;
+            else if (cmp < 0) current = current.LChild;
+            else current = current.RChild;
+        }
+
+        // If not found, return null
+        return null;
+
+    }
+
+
+
+
+    public int NoDVDs()
+	{
+        //To be completed by students
+        int totalDVDs = 0;
+
+        // Traverse the tree in-order and count the DVDs for each movie
+        Stack<BTreeNode> stack = new Stack<BTreeNode>();
+        BTreeNode current = root;
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.LChild;
+            }
+            current = stack.Pop();
+            totalDVDs += current.Movie.TotalCopies;
+            current = current.RChild;
+        }
+
+        return totalDVDs;
+    }
+
+
+    public IMovie[] ToArray()
+    {
+
+        //To be completed by students
+        IMovie[] movieArray = new IMovie[count];
+        int index = 0;
+        InOrderTraversal(root, ref movieArray, ref index);
+        return movieArray;
+
+    }
+
+    private void InOrderTraversal(BTreeNode? node, ref IMovie[] movieArray, ref int index)
+    {
+        if (node != null)
+        {
+            InOrderTraversal(node.LChild, ref movieArray, ref index);
+            movieArray[index] = node.Movie;
+            index++;
+            InOrderTraversal(node.RChild, ref movieArray, ref index);
+        }
+    }
+
+
+    public void Clear()
+	{
+
+		//To be completed by students
+		root = null;
+		count = 0;
+
+    }
+}
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class TestMovie
+{
+    public static void Main(string[] args)
+    {
+
+        //Testing Moive.cs
+        Console.WriteLine("Testing MovieCollection.cs \n\n" + "---------------------\n");
+
+        // create a new movie collection
+        IMovieCollection collection = new MovieCollection();
+
+        Console.WriteLine("Number of movies in moviecollection: " + collection.Number);
+
+        // add some movies to the collection
+        IMovie movie1 = new Movie("The Shark", MovieGenre.Drama, MovieClassification.M, 142, 2);
+        IMovie movie2 = new Movie("The Man", MovieGenre.Action, MovieClassification.M15Plus, 175, 2);
+        IMovie movie3 = new Movie("The Women", MovieGenre.Comedy, MovieClassification.M15Plus, 152, 1);
+        IMovie movie4 = new Movie("The Dog", MovieGenre.History, MovieClassification.G, 154, 1);
+        IMovie movie5 = new Movie("The Cat", MovieGenre.Western, MovieClassification.PG, 178, 2);
+        
+
+        bool inserted1 = collection.Insert(movie1);
+        bool inserted2 = collection.Insert(movie2);
+        bool inserted3 = collection.Insert(movie3);
+        bool inserted4 = collection.Insert(movie4);
+        bool inserted5 = collection.Insert(movie5);
+
+        Console.WriteLine("Number of movies in moviecollection after adding: " + collection.Number);
+
+        // try to add duplicate movies to the collection
+        bool inserted6 = collection.Insert(movie1);
+        bool inserted7 = collection.Insert(movie3);
+        bool inserted8 = collection.Insert(movie5);
+
+        Console.WriteLine("Number of movies in moviecollection after adding duplicate movies: " + collection.Number);
+
+        // delete a movie from the collection
+        bool deletedMovie = collection.Delete(movie3);
+        Console.WriteLine("The Women deleted from collection: {0}", deletedMovie);
+
+        // try to delete a non-existing movie from the collection
+        bool deletedMovie2 = collection.Delete(movie3);
+        Console.WriteLine("Non-existing movie deleted from collection: {0}", deletedMovie2);
+
+        Console.WriteLine("Number of movies in moviecollection after deleting: " + collection.Number);
+
+        // search for a movie in the collection
+        IMovie? foundMovie = collection.Search("The Cat");
+        Console.WriteLine("Search for 'The Cat': {0}", foundMovie != null ? "found" : "not found");
+
+        // calculate the total number of DVDs in the collection
+        int totalDVDs = collection.NoDVDs();
+        Console.WriteLine("Total number of DVDs in collection: {0}", totalDVDs);
+
+        // get an array of movies sorted by title
+        IMovie[] sortedMovies = collection.ToArray();
+        Console.WriteLine("Movies in collection sorted by title:");
+        foreach (IMovie movie in sortedMovies)
+        {
+            Console.WriteLine("- {0} ({1})", movie.Title, movie.Genre);
+        }
+        
+        // clear the collection
+        collection.Clear();
+        Console.WriteLine("Collection cleared: {0}", collection.IsEmpty());
+
+        Console.WriteLine(collection.Number); 
+    }
+}
